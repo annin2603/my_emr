0383bc76170443d80196648a0073074e19635250
HP002\HP002 <krmschool2512@gmail.com>
2026-04-16 17:28:55 +0900

    DBを作ってからPDOを使う ログイン状態をセッションにする ログイン成功時のif文を追加する予定

--- login-check.php
+++ login-check.php
@@ -1,4 +1,11 @@
+<?php session_start(); ?>
 <?php
+// $pdo = new PDO('mysql:host=db;dbname=medical_staff;charset=utf8',
+//                'staff',
+//                'password');
+// host=localhost ではなく host=db (ymlで決めた名前) にするのがDockerのコツです！
+//PDOはDBを作ってから有効にする
+
 // 送られてきたログイン情報を受け取る
 $user = $_POST['user_id'];
 $pass = $_POST['password'];
